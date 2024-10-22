747e113f3cda006d3730af1da6195568e83bc9b4
jan438 <jan438@users.noreply.github.com>
2024-10-22 14:15:47 +0200

    wait removed

--- Canvas/absolute_pos_flowable.py
+++ Canvas/absolute_pos_flowable.py
@@ -10,12 +10,10 @@ def mixed():
     my_canvas = canvas.Canvas("PDF/mixed_flowables.pdf", pagesize=letter)
     styles = getSampleStyleSheet()
     width, height = letter
-
     text = "Hello, I'm a Paragraph"
     para = Paragraph(text, style=styles["Normal"])
     para.wrapOn(my_canvas, width, height)
     para.drawOn(my_canvas, 20, 760)
-
     my_canvas.save()
 
 if __name__ == '__main__':
@@ -24,6 +22,5 @@ if __name__ == '__main__':
     if sys.platform[0] == 'w':
         path = "C:/Users/janbo/OneDrive/Documents/GitHub/ReportLAB"
     os.chdir(path)
-    key = input("Wait")
     mixed()
     key = input("Wait")
