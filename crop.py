d6628fe57573ee8468b10e38c85378a668c24b67
jan438 <jan438@users.noreply.github.com>
2026-06-15 13:21:00 +0200

    added

--- crop.py
+++ crop.py
@@ -1,7 +1,28 @@
 from reportlab.lib.pagesizes import A3, A4
 from reportlab.pdfgen import canvas
 import os
+import csv
+import sys
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from reportlab.graphics import renderPDF
+from reportlab.lib.colors import yellow, green, red, black, gray, white, HexColor, tan
+from reportlab.lib.units import inch, cm, mm
+from math import pi, cos, sin, radians, sqrt
 
+def scaleSVG(svgfile, scaling_factor):
+    svg_root = load_svg_file(svgfile)
+    svgRenderer = SvgRenderer(svgfile)
+    drawing = svgRenderer.render(svg_root)
+    scaling_x = scaling_factor
+    scaling_y = scaling_factor
+    drawing.width = drawing.minWidth() * scaling_x
+    drawing.height = drawing.height * scaling_y
+    drawing.scale(scaling_x, scaling_y)
+    return drawing
+    
 def create_pdf_template(filename, pagesize, title="Template"):
 
     try:
@@ -15,6 +36,7 @@ def create_pdf_template(filename, pagesize, title="Template"):
         # Optional: Draw border
         c.setLineWidth(1)
         c.rect(20, 20, width - 40, height - 40)
+        renderPDF.draw(scaleSVG("star.svg", 50.0), c, 0, 0)
 
         c.showPage()
         c.save()
